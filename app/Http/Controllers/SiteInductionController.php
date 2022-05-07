dbb47f84a732faea5ec2392db2503f93904395b9
scottmangles <scottmangles@hotmail.com>
2022-05-07 19:21:24 +0100

    Initial commit

---
+++ app/Http/Controllers/SiteInductionController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSiteInductionRequest;
+use App\Http\Requests\UpdateSiteInductionRequest;
+use App\Models\SiteInduction;
+
+class SiteInductionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreSiteInductionRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreSiteInductionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\SiteInduction  $siteInduction
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SiteInduction $siteInduction)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\SiteInduction  $siteInduction
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SiteInduction $siteInduction)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateSiteInductionRequest  $request
+     * @param  \App\Models\SiteInduction  $siteInduction
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateSiteInductionRequest $request, SiteInduction $siteInduction)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\SiteInduction  $siteInduction
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SiteInduction $siteInduction)
+    {
+        //
+    }
+}
